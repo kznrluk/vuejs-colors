e1d0a812d9fc007d25a4db86e646601d8531f27a
kznrluk <kznr.luk@gmail.com>
2018-03-04 19:56:41 +0900

    ソースを整理

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 // learn js freamwork server
 
-const handler = () => console.log(resJson.color)
 const server    = require('http').createServer(handler);
-const fs = require('fs')
-const EventEmitter = require('events').EventEmitter;
-const input = new EventEmitter();
+const fs        = require('fs')
+const EE        = require('events').EventEmitter;
+const input     = new EE();
 
 const colors = ['#0D47A1','#1B5E20','#d32f2f','#3D5AFE','#FFAB00','#9CCC65','#42A5F5']
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
 const changeColor = () => {
     resJson.color = colors[Math.floor(Math.random() * colors.length)]
 }
 
+const handler = () => console.log(resJson.color)
+let resJson = {};
+
 server.on('request', (req, res) => {
-    const response = data => {
-        res.writeHead(200,{ 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
+    const response = (data, contentType) => {
+        res.writeHead(200,{ 'Content-Type': contentType, 'Access-Control-Allow-Origin': '*' });
         res.write(data);
         res.end();
     }
 
-    if(req.url === '/REST/COLOR'){
-        response(JSON.stringify(resJson));
+    if(req.url === '/API/COLOR'){
+        response(JSON.stringify(resJson, 'text/plain'));
         changeColor();
     }
 });
 
-let resJson = {};
-
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
 process.stdin.on('data', chunk => {
     if(chunk.indexOf('SET') >= 0){
         resJson.color = chunk.split(/\/|\s/)[1]
